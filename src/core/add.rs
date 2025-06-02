8b6388335840426bd623e2f91137f8d4960f32b3
Nivaldo Melo <nivaldogmelo@gmail.com>
2025-06-02 14:48:33 -0300

    feat: Add default name to add command

--- src/core/add.rs
+++ src/core/add.rs
@@ -5,12 +5,29 @@ use inquire::Text;
 use crate::domain::Project;
 
 pub fn add(path: PathBuf) -> Result<Project, String> {
-    let name = Text::new("What's gonna be the project name?").prompt();
+    let default_name = path
+	.file_name()
+	.and_then(|name| name.to_str())
+	.unwrap_or("new_project")
+	.to_string();
+
+    let input_msg = format!(
+	"What's gonna be the project name? (default: {})",
+	default_name
+    );
+    let name = Text::new(&input_msg).prompt();
 
     match name {
 	Ok(name) => {
-	    println!("Project name: {}", name);
-	    let project = Project::new(name.clone(), path.clone());
+	    let mut project_name = String::new();
+	    if name.is_empty() {
+		project_name = default_name;
+	    } else {
+		project_name = name;
+	    }
+
+	    println!("Project name: {}", project_name);
+	    let project = Project::new(project_name, path.clone());
 	    Ok(project)
 	}
 	Err(e) => {
